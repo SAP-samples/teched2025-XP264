03169116aa7ede1c5ea49cec0f5a4068938f0e47
ptesny <rth@tiosouji.com>
2025-10-19 12:01:53 +0100

    Update main.tf

--- exercises/ex1/k8s-mt/modules/btp-mt/main.tf
+++ exercises/ex1/k8s-mt/modules/btp-mt/main.tf
@@ -111,7 +111,7 @@ resource "btp_subaccount_role_collection_assignment" "subaccount_platform_idp_gr
 #
 resource "btp_subaccount_role_collection_assignment" "subaccount_custom_idp_groups" {
 
-  depends_on           = [btp_subaccount_trust_configuration.custom_idp]
+  depends_on           = [btp_subaccount_subscription.custom_idp.faas-xp264-mt]
 
   for_each             = toset( "${var.admin_groups}" ) 
   subaccount_id        = data.btp_subaccount.context.id
@@ -127,6 +127,7 @@ locals {
 }
 
 resource "btp_subaccount_trust_configuration" "custom_idp" {
+
   subaccount_id     = data.btp_subaccount.context.id
   identity_provider = local.identity_provider
   name              = "${local.subaccount_domain}"
